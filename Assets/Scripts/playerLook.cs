1e97461548c88895c64e2dc1f2cb617a32d7db73
Isaak <imoore1@mail.pima.edu>
2018-10-18 14:23:30 -0700

    Added a more complex cameracontroller for more real looking

--- Assets/Scripts/playerLook.cs
+++ Assets/Scripts/playerLook.cs
@@ -7,9 +7,14 @@ public class playerLook : MonoBehaviour
     [SerializeField] private string mouseXInputName, mouseYInputName;
     [SerializeField] private float mouseSensitivity;
 
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp;
+
     private void Awake()
     {
         LockCursor();
+        xAxisClamp = 0.0f;
     }
 
 
@@ -28,6 +33,28 @@ public class playerLook : MonoBehaviour
         float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
-        transform.Rotate(-transform.right * mouseY);
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXRotation(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXRotation(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    private void ClampXRotation(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
     }
 }
